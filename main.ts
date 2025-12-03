3f0107e4e9b713f6e1b981ed3d8d226454dc126b
Ryan J. Blake <ryan.blake@gmail.com>
2025-12-03 04:14:36 -0500

    adding health check

--- main.ts
+++ main.ts
@@ -3,6 +3,13 @@ import { Client, GatewayIntentBits } from "discord.js";
 import { getAppConfig } from "@/config.ts";
 import { register } from "@/utils/register.ts";
 
+Deno.serve((req) => {
+  if (new URL(req.url).pathname === "/health") {
+    return new Response("OK", { status: 200 });
+  }
+  return new Response("Not Found", { status: 404 });
+});
+
 const { discordBotToken } = getAppConfig();
 const client = new Client({
   intents: [
@@ -14,7 +21,9 @@ const client = new Client({
   ],
 });
 
-await register("events", (event) => {
+// discord.js types fucking suck
+// deno-lint-ignore no-explicit-any
+await register("events", (event: any) => {
   if (event.once) {
     client.once(event.name, (...args) => event.execute(...args));
   } else {
