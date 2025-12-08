85de82f4c707914e3d4c57732b6c0d7580373a5a
Ryan J. Blake <ryan.blake@gmail.com>
2025-12-08 17:57:51 -0500

    adding pg with guild table

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import "@std/dotenv/load";
 import { Client, GatewayIntentBits } from "discord.js";
 import { getAppConfig } from "@/config.ts";
 import { register } from "@/utils/register.ts";
+import guildRepository from "@/repositories/guildRepository.ts";
 
 const { discordBotToken } = getAppConfig();
 const client = new Client({
@@ -25,3 +26,5 @@ await register("events", (event: any) => {
 });
 
 await client.login(discordBotToken);
+
+await guildRepository.registerGuilds(client.guilds);
---
+++ repositories/guildRepository.ts
@@ -0,0 +1,44 @@
+import type { GuildManager } from "discord.js";
+import type { PoolClient } from "pg";
+import { Pool } from "pg";
+
+class GuildRepository {
+  private pool: Pool;
+
+  constructor() {
+    this.pool = new Pool();
+  }
+
+  registerGuilds = async (guilds: GuildManager) => {
+    const register = async (guild_id: string, guild_name: string) => {
+      let client: PoolClient | undefined;
+
+      try {
+        client = await this.pool.connect();
+
+        await client.query(
+          "INSERT INTO guild (guild_id, guild_name) VALUES($1, $2) ON CONFLICT ON CONSTRAINT guild_pkey DO NOTHING;",
+          [guild_id, guild_name],
+        );
+      } catch (e) {
+        console.error(e);
+      } finally {
+        if (client) {
+          client.release();
+        }
+      }
+    };
+
+    for (const guild of guilds.cache.values()) {
+      const full_guild = guilds.cache.get(guild.id);
+
+      if (full_guild) {
+        await register(full_guild.id, full_guild.name);
+      }
+    }
+  };
+}
+
+const instance = new GuildRepository();
+
+export default instance;
