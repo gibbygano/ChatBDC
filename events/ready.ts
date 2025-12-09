e2ac552b2afe0fe152c252fc7d3a0bb1b494fd75
Ryan J. Blake <ryan.blake@gmail.com>
2025-12-09 00:52:57 -0500

    moved db write to ready

--- events/ready.ts
+++ events/ready.ts
@@ -1,10 +1,15 @@
-import { ActivityType, Client, Events, PresenceUpdateStatus } from "discord.js";
+import type { Client } from "discord.js";
+import { ActivityType, Events, PresenceUpdateStatus } from "discord.js";
+import guildRepository from "@/repositories/guildRepository.ts";
 
 export default {
   name: Events.ClientReady,
   once: true,
-  execute(client: Client) {
+  execute: async (client: Client) => {
     console.info(`Ready! Logged in as ${client.user?.tag}`);
+
+    await guildRepository.registerGuilds(client.guilds);
+
     client.user?.setPresence({
       activities: [{ name: "💤", type: ActivityType.Custom }],
       status: PresenceUpdateStatus.Idle,
