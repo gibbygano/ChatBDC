4a70427f5d655c2b9830ddb21ecc44fbff81f1e7
Ryan J. Blake <ryan.blake@gmail.com>
2025-12-19 16:39:16 -0500

    Adding Guild service

--- events/ready.ts
+++ events/ready.ts
@@ -4,12 +4,16 @@ import { ActivityType, Events, PresenceUpdateStatus } from "discord.js";
 import { GuildRepository } from "@/repositories/guildRepository.ts";
 import { PoolProvider } from "@/infrastructure/poolProvider.ts";
 import { registerQueueListeners } from "@/utils/register.ts";
+import { GuildService } from "@/services/guildService.ts";
 
 export default {
   name: Events.ClientReady,
   once: true,
   execute: async (client: Client) => {
-    const guild_repository = new GuildRepository(PoolProvider.instance);
+    // Register guilds
+    const guild_repository = new GuildService(
+      new GuildRepository(PoolProvider.instance),
+    );
     await guild_repository.registerGuilds(client.guilds);
 
     // Register all queue listeners
--- repositories/guildRepository.ts
+++ repositories/guildRepository.ts
@@ -1,10 +1,10 @@
 import type { IPoolProvider } from "@/infrastructure/poolProvider.ts";
-import type { GuildManager } from "discord.js";
+import type { Guild } from "discord.js";
 
 import { BaseRepository } from "./baseRepository.ts";
 
 export interface IGuildRepository {
-  registerGuilds(guilds: GuildManager): Promise<void>;
+  registerGuild(guild: Guild): Promise<boolean>;
 }
 
 export class GuildRepository extends BaseRepository
@@ -13,34 +13,17 @@ export class GuildRepository extends BaseRepository
     super(poolProvider);
   }
 
-  async registerGuilds(guilds: GuildManager) {
-    const register = async (guild_id: string, guild_name: string) => {
-      const prepared_statement = {
-        name: "register-guild",
-        text:
-          "INSERT INTO guild (guild_id, guild_name) VALUES($1, $2) ON CONFLICT ON CONSTRAINT guild_pkey DO NOTHING;",
-        values: [guild_id, guild_name],
-      };
-
-      const result = await this.queryWithSuccess(prepared_statement);
-
-      if (result) {
-        console.info(`> Registered guild ${guild_id}:${guild_name}`);
-      }
+  async registerGuild(guild: Guild) {
+    const prepared_statement = {
+      name: "register-guild",
+      text: `INSERT INTO guild (guild_id, guild_name) 
+             VALUES($1, $2) 
+             ON CONFLICT ON CONSTRAINT guild_pkey 
+             DO NOTHING
+         `,
+      values: [guild.id, guild.name],
     };
 
-    console.info(
-      "\nLooking for guilds to register.\n---------------------------------------------",
-    );
-
-    for (const guild of guilds.cache.values()) {
-      const full_guild = await guilds.cache.get(guild.id)?.fetch();
-
-      if (full_guild) {
-        await register(full_guild.id, full_guild.name);
-      }
-    }
-
-    console.info("---------------------------------------------\n");
+    return await this.queryWithSuccess(prepared_statement);
   }
 }
