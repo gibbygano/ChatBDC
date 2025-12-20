0dd728eb7b51eddfd713245a6d125a5f0cf7a9c0
Ryan J. Blake <ryan.blake@gmail.com>
2025-12-20 00:16:50 -0500

    random stuff to trigger hook (take 2)

--- interactions/stop.ts
+++ interactions/stop.ts
@@ -1,14 +1,11 @@
 import type { ChatInputCommandInteraction } from "discord.js";
+
 import { MediaService } from "@/services/mediaService.ts";
 
 const execute = async (interaction: ChatInputCommandInteraction) => {
   const member = await interaction.guild?.members.fetch(interaction.user.id);
   const voice_channel = member?.voice.channel;
-  await MediaService.instance.playMedia(
-    interaction,
-    voice_channel,
-    "stop",
-  );
+  await MediaService.instance.playMedia(interaction, voice_channel, "stop");
 };
 
 export { execute };
