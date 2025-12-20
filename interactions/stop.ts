f713d702cdac5b47770866b872b44e0e2fff7e02
Ryan J. Blake <ryan.blake@gmail.com>
2025-12-20 00:50:01 -0500

    Small change to trigger hook

--- interactions/stop.ts
+++ interactions/stop.ts
@@ -5,6 +5,7 @@ import { MediaService } from "@/services/mediaService.ts";
 const execute = async (interaction: ChatInputCommandInteraction) => {
   const member = await interaction.guild?.members.fetch(interaction.user.id);
   const voice_channel = member?.voice.channel;
+
   await MediaService.instance.playMedia(interaction, voice_channel, "stop");
 };
 
