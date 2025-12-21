be7e7f828bcf5bc524173b6b4d19bd6ebca3e129
Ryan J. Blake <ryan.blake@gmail.com>
2025-12-20 20:21:50 -0500

    add channel_id

--- repositories/messageRepository.ts
+++ repositories/messageRepository.ts
@@ -18,14 +18,15 @@ export class MessageRepository extends BaseRepository
     const prepared_statement = {
       name: "insert-message",
       text:
-        `INSERT INTO message_history(message_id, user_id, message_content, message_timestamp, guild_id) 
-         VALUES($1, $2, $3, to_timestamp($4), $5)`,
+        `INSERT INTO message_history(message_id, user_id, message_content, message_timestamp, guild_id, channel_id) 
+         VALUES($1, $2, $3, to_timestamp($4), $5, $6)`,
       values: [
         message.id,
         message.member?.user.id,
         message.content,
         message.createdTimestamp,
         message.guildId,
+        message.channelId,
       ],
     };
 
