43e5e2d8d65ec0e5515768307944267bdbaec890
Ryan J. Blake <ryan.blake@gmail.com>
2025-12-31 23:37:25 -0500

    Adding extra space for pretty print and line break

--- utils/replies.ts
+++ utils/replies.ts
@@ -39,8 +39,8 @@ const handle_reply = async (
     )
   ) {
     logger.log_warning(
-      "Can't reply to message. Missing message history permission flag",
-      JSON.stringify(interaction, null, 2),
+      "Can't reply to message. Missing message history permission flag\n",
+      JSON.stringify(interaction, null, 3),
     );
     return;
   }
@@ -91,8 +91,8 @@ const handle_file_reply = async (
     )
   ) {
     logger.log_warning(
-      "Can't reply to message. Missing message history permission flag",
-      JSON.stringify(interaction, null, 2),
+      "Can't reply to message. Missing message history permission flag\n",
+      JSON.stringify(interaction, null, 3),
     );
     return;
   }
