cad902ee146487867cd45e95be725c92c15cc257
Ryan J. Blake <ryan.blake@gmail.com>
2026-02-06 13:59:37 -0500

    spilling hur

--- events/reactionAdd.ts
+++ events/reactionAdd.ts
@@ -10,7 +10,7 @@ export default {
       await message.delete();
 
       logger.log_info(
-        "Nuking stupid emojie that I hate so much",
+        "Nuking stupid emoji that I hate so much",
         JSON.stringify(message.member?.user, null, 3),
         JSON.stringify(reaction.emoji, null, 3),
       );
