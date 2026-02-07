2ede4cfdfd2d99ad7d4b82cddc60a5bd935f0e4b
Ryan J. Blake <ryan.blake@gmail.com>
2026-02-07 16:22:19 -0500

    Take 2

--- events/reactionAdd.ts
+++ events/reactionAdd.ts
@@ -1,17 +1,17 @@
-import type { Message, MessageReaction } from "discord.js";
+import type { MessageReaction, User } from "discord.js";
 
 import { Events } from "discord.js";
 import logger from "@logging";
 
 export default {
   name: Events.MessageReactionAdd,
-  execute: async (reaction: MessageReaction, message: Message) => {
+  execute: async (reaction: MessageReaction, user: User) => {
     if (reaction.emoji.name === "twerk") {
-      await message.delete();
+      await reaction.remove();
 
       logger.log_info(
         "Nuking stupid emoji that I hate so much",
-        JSON.stringify(message.member?.user, null, 3),
+        JSON.stringify(user, null, 3),
         JSON.stringify(reaction.emoji, null, 3),
       );
     }
