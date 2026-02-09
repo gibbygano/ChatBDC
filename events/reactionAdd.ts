5d1d4c100a139228ea95682da4b8775bbf0e7e1c
Ryan J. Blake <ryan.blake@gmail.com>
2026-02-08 23:00:48 -0500

    handling error for reaction permissions

--- events/reactionAdd.ts
+++ events/reactionAdd.ts
@@ -7,13 +7,21 @@ export default {
   name: Events.MessageReactionAdd,
   execute: async (reaction: MessageReaction, user: User) => {
     if (reaction.emoji.name === "twerk") {
-      await reaction.remove();
-
       logger.log_info(
         "Nuking stupid emoji that I hate so much",
         JSON.stringify(user, null, 3),
         JSON.stringify(reaction.emoji, null, 3),
       );
+
+      try {
+        await reaction.remove();
+      } catch (error) {
+        logger.log_error(
+          "Could not remove reaction",
+          error,
+          JSON.stringify(reaction, null, 3),
+        );
+      }
     }
   },
 };
